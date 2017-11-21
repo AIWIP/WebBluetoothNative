5770937121ebbfc652f52240e7a50412cb2058d2
James Campbell <james@supmenow.com>
2017-11-21 21:00:42 +0000

    Creating class.

--- src/electron/MainProcessImplementation.js
+++ src/electron/MainProcessImplementation.js
@@ -1,9 +1,10 @@
 const noble = require('noble');
 
-module.exports = {
-    requestLEScan: () => {
-        console.log('Scan')
+class MainProcessImplementation {
 
+    requestLEScan() {
+        console.log('Scan')
+        
         noble.on('stateChange', function(state) {
             console.log(state);
         });
