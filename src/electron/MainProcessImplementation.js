fca40ce3f61c47019e99a31bae47b8a17812a005
James Campbell <james@supmenow.com>
2017-11-21 21:32:38 +0000

    Preparing to implementing the actual classes which handle the magic.

--- src/electron/MainProcessImplementation.js
+++ src/electron/MainProcessImplementation.js
@@ -1,14 +1,29 @@
 const noble = require('noble');
 
+class BluetoothHostContext {
+    constructor() {
+        this.leRequests = []
+    }
+}
+
 class MainProcessImplementation {
 
-    requestLEScan() {
-        console.log('Scan')
-        
-        noble.on('stateChange', function(state) {
-            console.log(state);
-        });
+    constructor() {
+        console.log('Make Main')
 
+        this.poweredOn = false
+        this.context = new BluetoothHostContext()
+
+        noble.on('stateChange', function(state) {
+            console.log('Bluetooth State Changed: ' + state);
+
+            this.poweredOn = (state == 'poweredOn')
+        });
+    }
+
+    requestLEScan() {
+        console.log('Requesting LE Scan')
+        
         noble.on('discover', function(peripheral) {
             console.log(peripheral);
         });
