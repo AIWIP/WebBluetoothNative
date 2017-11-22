b3f208857706c435e6674f57daa504e2f6e1adb9
James Campbell <james@supmenow.com>
2017-11-22 09:08:52 +0000

    Basic listening implemented.

--- src/electron/RendererProcessImplementation.js
+++ src/electron/RendererProcessImplementation.js
@@ -1,10 +1,6 @@
 const { remote } = require('electron');
 const bluetooth = remote.require('./mainProcessImplementation');
 
-const EventTarget = require('event-target-shim').EventTarget;
-const Event = require('./event');
-const { ipcRenderer } = require('electron')
-
 module.exports = function setupWebBluetoothPolyfill(navigator) {
     const hasBluetoothSupport = (navigator.bluetooth !== undefined);
     const hasBluetoothLESupport = (navigator.bluetooth.requestLEScan !== undefined);
@@ -12,14 +8,11 @@ module.exports = function setupWebBluetoothPolyfill(navigator) {
     console.log('Browser has bluetooth: ' + hasBluetoothSupport)
     console.log('Browser has bluetooth LE support: ' + hasBluetoothLESupport)
 
-    if (!hasBluetoothSupport) {
-        console.log('Polyfilling Bluetooth Support')
-        navigator.bluetooth.requestDevice = bluetooth.requestDevice
-    }
+    navigator.bluetooth.requestLEScan = bluetooth.requestLEScan
 
-    if (!hasBluetoothLESupport) {
-        console.log('Polyfilling Bluetooth LE Support')
-        navigator.bluetooth.requestLEScan = bluetooth.requestLEScan
+    for (var x in bluetooth) {
+        console.log(x)
+        navigator.bluetooth[x] = bluetooth[x]
     }
 
     return navigator
